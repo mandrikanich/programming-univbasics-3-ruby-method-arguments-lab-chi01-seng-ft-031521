94628f85af6ce0fff2a9da16c7632deab4994012
Michael Andrikanich <mandrikanich@gmail.com>
2021-03-01 23:00:52 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,13 +6,11 @@ def introduction_with_language (name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end 
 
-def introduction_with_language_optional (name, language)
+def introduction_with_language_optional (name, language = "Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+  end
  
-greeting_programmer("Maria", "Ruby")
-# > Hello, Maria. We heard you are a great Ruby programmer.
- 
-greeting_programmer("Steven", "Elixir")
-# > Hello, Steven. We heard you are a great Elixir programmer.
+
 
 
 
